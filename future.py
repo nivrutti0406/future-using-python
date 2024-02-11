7978d402783c4c4156b30847db52c5c7c699f2f5
nivrutti0406 <159710492+nivrutti0406@users.noreply.github.com>
2024-02-11 07:49:34 -0800

    Create future.py
    Signed-off-by: nivrutti0406 <159710492+nivrutti0406@users.noreply.github.com>

---
+++ future.py
@@ -0,0 +1,152 @@
+import time
+p=int(input("enter password"))#9922
+
+if(p==9922):
+    print("wait please!!")
+    time.sleep(2)
+    print("                🤩welcome to future.com🤩    ")
+    time.sleep(4)
+    n=str(input("enter any char"))
+    if(n=="a"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=s")
+    elif(n=="b"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=s")
+    
+    
+    elif(n=="c"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=f")
+    elif(n=="d"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=ra")
+    
+    elif(n=="e"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=b")
+    elif(n=="f"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=p")
+    elif(n=="g"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=r")
+    elif(n=="h"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name starts from=s")
+    elif(n=="i"):
+        print("prossing")
+        time.sleep(5)
+        print("your future wife name starts from=k")
+    
+    elif(n=="j"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =n")
+
+
+    elif(n=="k"):
+        print("processing")
+        time.sleep(5)
+        print("you are not marry in future")
+    elif(n=="l"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =c")
+    elif(n=="m"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =v")
+    elif(n=="o"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =l")
+
+    elif(n=="n"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =c")
+    elif(n=="o"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =x")
+    elif(n=="p"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =e")
+
+
+    elif(n=="q"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =l")
+    elif(n=="r"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =i")
+    elif(n=="s"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =h")
+    elif(n=="t"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =s")
+
+
+    elif(n=="u"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =d")
+    elif(n=="v"):
+        print("processing")
+        time.sleep(5)
+        print("you are not marry in future")
+    elif(n=="w"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =p")
+    elif(n=="x"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =o")
+    elif(n=="y"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =l")
+    elif(n=="z"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =g")
+    elif(n=="x"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =d")
+    elif(n=="a"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =o")
+    elif(n=="l"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =t")
+    elif(n=="s"):
+        print("processing")
+        time.sleep(5)
+        print("your future wife name is =s")
+
+    else:
+        print("you are staying always single")
+else:
+    print("wrong password")
+print("_____")    
+print("-----------------------")   
+print(" 🙏t  h  a  n  k_ Y  o  u..🙏")
